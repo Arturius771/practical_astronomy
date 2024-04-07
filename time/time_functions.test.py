badcb039f92fc3f22f03870d6e081e077adfd664
ArturFoden <fodena@tcd.ie>
2024-04-07 01:17:36 +0100

    formatting

--- time/time_functions.test.py
+++ time/time_functions.test.py
@@ -5,19 +5,16 @@ class TestDateMethods(unittest.TestCase):
 
   def test_date_of_easter(self):
     msg = 'test_date_of_easter fail'
+
     self.assertEqual(time_functions.date_of_easter(2009), (12,4,2009), msg)
-
     self.assertEqual(time_functions.date_of_easter(2010), (4,4,2010), msg)
-
     self.assertEqual(time_functions.date_of_easter(2024), (31,3,2024), msg)
 
   def test_date_to_day_number(self):
     msg = 'test_date_to_day_number fail'
 
     self.assertEqual(time_functions.date_to_day_number(1,1,True), 1, msg)
-
     self.assertEqual(time_functions.date_to_day_number(12,31,True), 366, msg)
-
     self.assertEqual(time_functions.date_to_day_number(12,31,False), 365, msg)
 
   def test_greenwich_date_to_julian_date(self):
