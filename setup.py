1e91d7f94ebe28504add88f298b90e4766a32031
Artur Foden <adfoden@gmail.com>
2026-05-10 02:51:25 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="af_practical_astronomy",
-    version="1.0.9",
+    version="1.1.0",
     description="A practical astronomy library",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
